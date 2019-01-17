3f5611b31bf6d80c8d31f0c01d08ca426f493cbc
Omar <ramiritoramo@gmail.com>
2019-01-17 16:06:35 +0000

    Change STATICFILES_STORAGE

--- yawm/settings.py
+++ yawm/settings.py
@@ -133,7 +133,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
