9179803272eccff68be1f96eb484ece94820f574
Omar <ramiritoramo@gmail.com>
2019-01-17 16:08:18 +0000

    Set STATIC_ROOT

--- yawm/settings.py
+++ yawm/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
