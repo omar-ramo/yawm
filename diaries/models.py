6eec44a1851f66fc19550eb4afb5fb46d37ae880
omar-ramo <ramiritoramo@gmail.com>
2019-02-20 13:49:21 +0000

    Add `likes_count` and `comments_count` to `Diary` model so that we wont have to use `DiaryLike` model every time. (anaconda plugin replaced every tab with 4 spaces)

--- diaries/models.py
+++ diaries/models.py
@@ -13,224 +13,221 @@ from core.utils import get_image_upload_path, generate_random_string
 
 class DiaryQuerySet(models.QuerySet):
 
-	def active(self, user):
-		if not user.is_authenticated:
-			qs = self.filter(is_visible=Diary.ALL_CHOICE)
-		else:
-			# User can see his non-visible diaries.
-			qs = self.filter(
-				Q(author=user.profile) |
-				Q(is_visible=Diary.ALL_CHOICE))
-			qs = qs.distinct()
-		qs = qs.select_related('author')
-		return qs
+    def active(self, user):
+        if not user.is_authenticated:
+            qs = self.filter(is_visible=Diary.ALL_CHOICE)
+        else:
+            # User can see his non-visible diaries.
+            qs = self.filter(
+                Q(author=user.profile) |
+                Q(is_visible=Diary.ALL_CHOICE))
+            qs = qs.distinct()
+        qs = qs.select_related('author')
+        return qs
 
-	def with_likes_and_comments_count(self):
-		qs = self.annotate(
-			likes_count=Count(F('likes'), distinct=True),
-			comments_count=Count(F('comments'), distinct=True))
-		return qs
+    def from_followed_profiles(self, profile):
+        followed_profiles = profile.followed_profiles.all()
+        qs = self.filter(author__in=followed_profiles)
+        return qs
 
-	def from_followed_profiles(self, profile):
-		followed_profiles = profile.followed_profiles.all()
-		qs = self.filter(author__in=followed_profiles)
-		qs = qs.with_likes_and_comments_count()
-		return qs
-
-	def popular(self):
-		qs = self.with_likes_and_comments_count()
-		qs = qs.annotate(ranking_factor=F('comments_count') + F('likes_count'))
-		qs = qs.order_by('-ranking_factor')
-		return qs
+    def popular(self):
+        qs = self.annotate(
+            ranking_factor=F('comments_count') + F('likes_count')
+        )
+        qs = qs.order_by('-ranking_factor')
+        return qs
 
 
 class Diary(models.Model):
-	ALL_CHOICE = 'all'
-	NO_ONE_CHOICE = 'no_one'
-	VISIBILITY_CHOICES = (
-		(ALL_CHOICE, 'All'),
-		(NO_ONE_CHOICE, 'No One'))
-	COMMENTABLE_CHOICES = (
-		(ALL_CHOICE, 'All'),
-		(NO_ONE_CHOICE, 'No One'))
-	ANGRY_FEELING = '0'
-	HAPPY_FEELING = '1'
-	EXCITED_FEELING = '2'
-	SAD_FEELING = '3'
-	LOVE_FEELING = 'LOVE'
-	SATISFIED_FEELING = 'SATISFIED'
-	MAD_FEELING = 'MAD'
-	TIRED_FEELING = 'TIRED'
-	SURPRISED_FEELING = 'SURPRISED'
-	AFRAID_FEELING = 'AFRAID'
-	FEELINGS_CHOICES = (
-		(ANGRY_FEELING, 'Angry'),
-		(HAPPY_FEELING, 'Happy'),
-		(EXCITED_FEELING, 'Excited'),
-		(SAD_FEELING, 'Sad'),
-		(LOVE_FEELING, 'Love'),
-		(SATISFIED_FEELING, 'Satisfied'),
-		(MAD_FEELING, 'Mad'),
-		(TIRED_FEELING, 'Tired'),
-		(SURPRISED_FEELING, 'Surprised'),
-		(AFRAID_FEELING, 'Afraid'),)
+    ALL_CHOICE = 'all'
+    NO_ONE_CHOICE = 'no_one'
+    VISIBILITY_CHOICES = (
+        (ALL_CHOICE, 'All'),
+        (NO_ONE_CHOICE, 'No One'))
+    COMMENTABLE_CHOICES = (
+        (ALL_CHOICE, 'All'),
+        (NO_ONE_CHOICE, 'No One'))
+    ANGRY_FEELING = '0'
+    HAPPY_FEELING = '1'
+    EXCITED_FEELING = '2'
+    SAD_FEELING = '3'
+    LOVE_FEELING = 'LOVE'
+    SATISFIED_FEELING = 'SATISFIED'
+    MAD_FEELING = 'MAD'
+    TIRED_FEELING = 'TIRED'
+    SURPRISED_FEELING = 'SURPRISED'
+    AFRAID_FEELING = 'AFRAID'
+    FEELINGS_CHOICES = (
+        (ANGRY_FEELING, 'Angry'),
+        (HAPPY_FEELING, 'Happy'),
+        (EXCITED_FEELING, 'Excited'),
+        (SAD_FEELING, 'Sad'),
+        (LOVE_FEELING, 'Love'),
+        (SATISFIED_FEELING, 'Satisfied'),
+        (MAD_FEELING, 'Mad'),
+        (TIRED_FEELING, 'Tired'),
+        (SURPRISED_FEELING, 'Surprised'),
+        (AFRAID_FEELING, 'Afraid'),)
 
-	title = models.CharField(max_length=255)
-	# Size of slug is bigger than size of title
-	# Because we want to add random string at the end
-	slug = models.SlugField(max_length=275, blank=True, allow_unicode=True)
-	content = RichTextUploadingField()
-	description = models.CharField(max_length=255, null=True, blank=True)
-	image = models.ImageField(
-		upload_to=get_image_upload_path,
-		null=True,
-		blank=True)
-	is_visible = models.CharField(
-		help_text='Who can see this diary?',
-		max_length=7,
-		choices=VISIBILITY_CHOICES,
-		default=ALL_CHOICE)
-	is_commentable = models.CharField(
-		help_text='Who can comment on this diary?',
-		choices=COMMENTABLE_CHOICES,
-		max_length=7,
-		default=ALL_CHOICE)
-	feeling = models.CharField(
-		help_text='How do you feel?',
-		max_length=15,
-		choices=FEELINGS_CHOICES,
-		null=True,
-		blank=True)
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    # Size of slug is bigger than size of title
+    # Because we want to add random string at the end
+    slug = models.SlugField(max_length=275, blank=True, allow_unicode=True)
+    content = RichTextUploadingField()
+    description = models.CharField(max_length=255, null=True, blank=True)
+    image = models.ImageField(
+        upload_to=get_image_upload_path,
+        null=True,
+        blank=True)
+    is_visible = models.CharField(
+        help_text='Who can see this diary?',
+        max_length=7,
+        choices=VISIBILITY_CHOICES,
+        default=ALL_CHOICE)
+    is_commentable = models.CharField(
+        help_text='Who can comment on this diary?',
+        choices=COMMENTABLE_CHOICES,
+        max_length=7,
+        default=ALL_CHOICE)
+    feeling = models.CharField(
+        help_text='How do you feel?',
+        max_length=15,
+        choices=FEELINGS_CHOICES,
+        null=True,
+        blank=True)
+    is_deleted = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-	likes = models.ManyToManyField(
-		Profile,
-		through='DiaryLike',
-		through_fields=('diary', 'user'))
-	author = models.ForeignKey(
-		Profile,
-		related_name='written_diaries',
-		on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+        Profile,
+        through='DiaryLike',
+        through_fields=('diary', 'user'))
+    likes_count = models.PositiveIntegerField(default=0)
+    comments_count = models.PositiveIntegerField(default=0)
+    author = models.ForeignKey(
+        Profile,
+        related_name='written_diaries',
+        on_delete=models.CASCADE)
 
-	objects = DiaryQuerySet.as_manager()
+    objects = DiaryQuerySet.as_manager()
 
-	class Meta:
-		verbose_name_plural = 'diaries'
-		ordering = ['-created_on']
+    class Meta:
+        verbose_name_plural = 'diaries'
+        ordering = ['-created_on']
 
-	def __str__(self):
-		return self.title
+    def __str__(self):
+        return self.title
 
-	def save(self, *args, **kwargs):
-		if(not self.id):
-			new_slug = '{}-{}'.format(
-				slugify(self.title, allow_unicode=True),
-				generate_random_string())
+    def save(self, *args, **kwargs):
+        if(not self.id):
+            new_slug = '{}-{}'.format(
+                slugify(self.title, allow_unicode=True),
+                generate_random_string())
 
-			while Diary.objects.filter(slug=new_slug).exists():
-				new_slug = '{}-{}'.format(
-					slugify(self.title, allow_unicode=True),
-					generate_random_string())
+            while Diary.objects.filter(slug=new_slug).exists():
+                new_slug = '{}-{}'.format(
+                    slugify(self.title, allow_unicode=True),
+                    generate_random_string())
 
-			self.slug = new_slug
-		self.description = ' '.join(self.content[:255].split(' ')[:-1])
-		return super(Diary, self).save(*args, **kwargs)
+            self.slug = new_slug
+        self.description = ' '.join(self.content[:255].split(' ')[:-1])
+        return super(Diary, self).save(*args, **kwargs)
 
-	def get_absolute_url(self):
-		return reverse(
-			'diaries:diary_detail',
-			kwargs={'diary_slug': self.slug})
+    def get_absolute_url(self):
+        return reverse(
+            'diaries:diary_detail',
+            kwargs={'diary_slug': self.slug})
 
 
 class DiaryLike(models.Model):
-	user = models.ForeignKey(
-		Profile,
-		on_delete=models.CASCADE,
-		related_name='liked_diaries')
-	diary = models.ForeignKey(
-		Diary,
-		on_delete=models.CASCADE,
-		related_name='diary_likes')
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='liked_diaries')
+    diary = models.ForeignKey(
+        Diary,
+        on_delete=models.CASCADE,
+        related_name='diary_likes')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		unique_together = ['user', 'diary']
+    class Meta:
+        unique_together = ['user', 'diary']
 
-	def __str__(self):
-		return '{} : {} ({})'.format(self.user, self.diary, self.created_on)
+    def __str__(self):
+        return '{} : {} ({})'.format(self.user, self.diary, self.created_on)
 
 
 class Comment(models.Model):
-	content = models.TextField()
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-	diary = models.ForeignKey(
-		Diary,
-		related_name='comments',
-		on_delete=models.CASCADE)
-	likes = models.ManyToManyField(
-		Profile,
-		related_name='liked_comments',
-		through='CommentLike',
-		through_fields=('comment', 'user'))
-	author = models.ForeignKey(
-		Profile,
-		related_name='written_comments',
-		on_delete=models.CASCADE)
+    diary = models.ForeignKey(
+        Diary,
+        related_name='comments',
+        on_delete=models.CASCADE)
+    likes = models.ManyToManyField(
+        Profile,
+        related_name='liked_comments',
+        through='CommentLike',
+        through_fields=('comment', 'user'))
+    author = models.ForeignKey(
+        Profile,
+        related_name='written_comments',
+        on_delete=models.CASCADE)
 
-	def __str__(self):
-		return '{}... ({})'.format(self.content, self.author)
+    def __str__(self):
+        return '{}... ({})'.format(self.content, self.author)
 
 
 class CommentLike(models.Model):
-	user = models.ForeignKey(
-		Profile,
-		on_delete=models.CASCADE,
-		related_name='comment_likes')
-	comment = models.ForeignKey(
-		Comment,
-		on_delete=models.CASCADE,
-		related_name='comment_likes')
-	created_on = models.DateTimeField(auto_now_add=True)
-	updated_on = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name='comment_likes')
+    comment = models.ForeignKey(
+        Comment,
+        on_delete=models.CASCADE,
+        related_name='comment_likes')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
-	def __str__(self):
-		return '{} : {} ({})'.format(
-			self.user,
-			self.comment,
-			self.created_on)
+    def __str__(self):
+        return '{} : {} ({})'.format(
+            self.user,
+            self.comment,
+            self.created_on)
 
 
 def diary_like_create_handler(sender, instance, created, **kwargs):
-	if instance.user != instance.diary.author:
-		notify.send(
-			sender=instance.user,
-			recipient=instance.diary.author.user,
-			target=instance.diary,
-			verb='Liked')
+    if instance.user != instance.diary.author:
+        notify.send(
+            sender=instance.user,
+            recipient=instance.diary.author.user,
+            target=instance.diary,
+            verb='Liked')
 
 
 post_save.connect(diary_like_create_handler, sender=DiaryLike)
 
 
 def comment_create_handler(sender, instance, created, **kwargs):
-	if instance.author != instance.diary.author:
-		notify.send(
-			sender=instance.author,
-			recipient=instance.diary.author.user,
-			target=instance.diary,
-			verb='Commented on')
+    if instance.author != instance.diary.author:
+        notify.send(
+            sender=instance.author,
+            recipient=instance.diary.author.user,
+            target=instance.diary,
+            verb='Commented on')
 
 
 post_save.connect(comment_create_handler, sender=Comment)
 
 
 def delete_notifications_when_diary_is_deleted(sender, instance, **kwargs):
-	qs = Notification.objects.filter(target_object_id=instance.id)
-	qs.delete()
+    qs = Notification.objects.filter(target_object_id=instance.id)
+    qs.delete()
 
 
 post_delete.connect(delete_notifications_when_diary_is_deleted, sender=Diary)
--- diaries/views.py
+++ diaries/views.py
@@ -1,15 +1,15 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.db.models import Q
+from django.db.models import Q, F
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import (
-	View,
-	ListView,
-	DetailView,
-	DeleteView,
-	UpdateView,
-	CreateView)
+    View,
+    ListView,
+    DetailView,
+    DeleteView,
+    UpdateView,
+    CreateView)
 from django.urls import reverse
 from notifications.views import NotificationViewList
 from notifications.models import Notification
@@ -23,219 +23,230 @@ DIARIES_PER_PAGE = 9
 
 
 class DiaryListView(ListView):
-	model = Diary
-	paginate_by = DIARIES_PER_PAGE
-	template_name = 'diaries/diary_list.html'
-	context_object_name = 'diaries'
-	order_by = None
+    model = Diary
+    paginate_by = DIARIES_PER_PAGE
+    template_name = 'diaries/diary_list.html'
+    context_object_name = 'diaries'
+    order_by = None
 
-	def get_queryset(self):
-		order_by = self.order_by
-		if order_by == 'popularity':
-			qs = self.model.objects.popular()
-		elif order_by == 'discover':
-			qs = self.model.objects.with_likes_and_comments_count()
-		else:
-			if self.request.user.is_authenticated:
-				# User will only see the diaries of people he follows + his own
-				# I will use .active(self.request.user) here because the
-				# Resulting qs of .union() doesn't allow oher operations
-				# other than: count, order_by, values,values_list
-				followed_profiles_diaries = Diary.objects\
-					.from_followed_profiles(self.request.user.profile)\
-					.order_by().active(self.request.user)
+    def get_queryset(self):
+        order_by = self.order_by
+        if order_by == 'popularity':
+            qs = self.model.objects.popular()
+        elif order_by == 'discover':
+            qs = self.model.objects.all()
+        else:
+            if self.request.user.is_authenticated:
+                # User will only see the diaries of people he follows + his own
+                # I will use .active(self.request.user) here because the
+                # Resulting qs of .union() doesn't allow oher operations
+                # other than: count, order_by, values,values_list
+                followed_profiles_diaries = Diary.objects\
+                    .from_followed_profiles(self.request.user.profile)\
+                    .order_by().active(self.request.user)
 
-				current_profile_diaries = Diary.objects\
-					.with_likes_and_comments_count().filter(
-						author=self.request.user.profile).order_by().active(
-							self.request.user)
+                current_profile_diaries = Diary.objects.filter(
+                    author=self.request.user.profile
+                ).order_by().active(
+                    self.request.user
+                )
 
-				qs = followed_profiles_diaries.union(current_profile_diaries)
-				qs = qs.order_by('-created_on')
-			else:
-				qs = self.model.objects.with_likes_and_comments_count()
-		qs = qs.active(self.request.user)
-		return qs
+                qs = followed_profiles_diaries.union(current_profile_diaries)
+                qs = qs.order_by('-created_on')
+            else:
+                qs = self.model.objects.all()
+        qs = qs.active(self.request.user)
+        return qs
 
 
 class DiaryDetailView(DetailView):
-	model = Diary
-	template_name = 'diaries/diary_detail.html'
-	context_object_name = 'diary'
-	slug_field = 'slug'
-	slug_url_kwarg = 'diary_slug'
+    model = Diary
+    template_name = 'diaries/diary_detail.html'
+    context_object_name = 'diary'
+    slug_field = 'slug'
+    slug_url_kwarg = 'diary_slug'
 
-	def get_object(self):
-		qs = self.model.objects.with_likes_and_comments_count()
-		qs = qs.filter(slug=self.kwargs[self.slug_url_kwarg])
-		qs = qs.active(self.request.user)
-		obj = qs.first()
-		if obj is None:
-			raise Http404()
-		return obj
+    def get_object(self):
+        qs = self.model.objects.all()
+        qs = qs.filter(slug=self.kwargs[self.slug_url_kwarg])
+        qs = qs.active(self.request.user)
+        obj = qs.first()
+        if obj is None:
+            raise Http404()
+        return obj
 
-	def get_context_data(self, **kwargs):
-		cx = super(DiaryDetailView, self).get_context_data(**kwargs)
+    def get_context_data(self, **kwargs):
+        cx = super(DiaryDetailView, self).get_context_data(**kwargs)
 
-		comment_form = None
-		if self.object.is_commentable == Diary.ALL_CHOICE:
-			comment_form = CommentForm()
-		cx['comment_form'] = comment_form
+        comment_form = None
+        if self.object.is_commentable == Diary.ALL_CHOICE:
+            comment_form = CommentForm()
+        cx['comment_form'] = comment_form
 
-		return cx
+        return cx
 
 
 class DiaryCreateView(LoginRequiredMixin, CreateView):
-	form_class = DiaryForm
-	template_name = 'diaries/diary_create.html'
-	context_object_name = 'diary'
+    form_class = DiaryForm
+    template_name = 'diaries/diary_create.html'
+    context_object_name = 'diary'
 
-	def get_success_url(self):
-		messages.success(self.request, 'Your Diary was created successfly.')
-		return self.object.get_absolute_url()
+    def get_success_url(self):
+        messages.success(self.request, 'Your Diary was created successfly.')
+        return self.object.get_absolute_url()
 
-	def form_valid(self, form):
-		form.instance.author = self.request.user.profile
-		return super().form_valid(form)
+    def form_valid(self, form):
+        form.instance.author = self.request.user.profile
+        return super().form_valid(form)
 
 
 class DiaryUpdateView(LoginRequiredMixin, UpdateView):
-	model = Diary
-	form_class = DiaryForm
-	template_name = 'diaries/diary_update.html'
-	slug_url_kwarg = 'diary_slug'
-	context_object_name = 'diary'
+    model = Diary
+    form_class = DiaryForm
+    template_name = 'diaries/diary_update.html'
+    slug_url_kwarg = 'diary_slug'
+    context_object_name = 'diary'
 
-	def get_success_url(self):
-		messages.success(self.request, 'Your Diary was updated successfly.')
-		return self.object.get_absolute_url()
+    def get_success_url(self):
+        messages.success(self.request, 'Your Diary was updated successfly.')
+        return self.object.get_absolute_url()
 
-	def get_object(self):
-		obj = get_object_or_404(
-			self.model,
-			slug=self.kwargs[self.slug_url_kwarg])
-		if self.request.user.profile == obj.author:
-			return obj
-		raise Http404()
+    def get_object(self):
+        obj = get_object_or_404(
+            self.model,
+            slug=self.kwargs[self.slug_url_kwarg])
+        if self.request.user.profile == obj.author:
+            return obj
+        raise Http404()
 
 
 class DiaryDeleteView(LoginRequiredMixin, DeleteView):
-	model = Diary
-	template_name = 'diaries/diary_delete.html'
-	slug_field = 'slug'
-	slug_url_kwarg = 'diary_slug'
+    model = Diary
+    template_name = 'diaries/diary_delete.html'
+    slug_field = 'slug'
+    slug_url_kwarg = 'diary_slug'
 
-	def get_success_url(self):
-		messages.warning(self.request, 'Your Diary was deleted successfly.')
-		return reverse('diaries:diary_list')
+    def get_success_url(self):
+        messages.warning(self.request, 'Your Diary was deleted successfly.')
+        return reverse('diaries:diary_list')
 
-	def get_object(self):
-		obj = get_object_or_404(
-			self.model,
-			slug=self.kwargs[self.slug_url_kwarg])
-		if self.request.user.profile == obj.author:
-			return obj
-		raise Http404()
+    def get_object(self):
+        obj = get_object_or_404(
+            self.model,
+            slug=self.kwargs[self.slug_url_kwarg])
+        if self.request.user.profile == obj.author:
+            return obj
+        raise Http404()
 
 
 class DiaryLikeView(LoginRequiredMixin, View):
-	def get(self, request, diary_slug):
-		diary = get_object_or_404(Diary, slug=diary_slug)
-		return redirect(diary)
+    def get(self, request, diary_slug):
+        diary = get_object_or_404(Diary, slug=diary_slug)
+        return redirect(diary)
 
-	def post(self, request, diary_slug):
-		diary = get_object_or_404(
-			Diary,
-			slug=diary_slug,
-			is_visible=Diary.ALL_CHOICE)
+    def post(self, request, diary_slug):
+        diary = get_object_or_404(
+            Diary,
+            slug=diary_slug,
+            is_visible=Diary.ALL_CHOICE)
 
-		dl, created = DiaryLike.objects.get_or_create(
-			diary=diary,
-			user=request.user.profile)
-		if not created:
-			dl.delete()
-		return redirect(diary)
+        dl, created = DiaryLike.objects.get_or_create(
+            diary=diary,
+            user=request.user.profile)
+        if created:
+            diary.likes_count = F('likes_count') + 1
+        else:
+            dl.delete()
+            diary.likes_count = F('likes_count') - 1
+        diary.save()
+        return redirect(diary)
 
 
 class CommentCreateView(LoginRequiredMixin, View):
-	form_class = CommentForm
+    form_class = CommentForm
 
-	def get(self, *args, **kwargs):
-		diary = get_object_or_404(
-			Diary,
-			slug=self.kwargs['diary_slug'],
-			is_visible=Diary.ALL_CHOICE)
-		return redirect(diary)
+    def get(self, *args, **kwargs):
+        diary = get_object_or_404(
+            Diary,
+            slug=self.kwargs['diary_slug'],
+            is_visible=Diary.ALL_CHOICE)
+        return redirect(diary)
 
-	def post(self, *args, **kwargs):
-		diary = get_object_or_404(
-			Diary,
-			slug=self.kwargs['diary_slug'],
-			is_visible=Diary.ALL_CHOICE,
-			is_commentable=Diary.ALL_CHOICE)
+    def post(self, *args, **kwargs):
+        diary = get_object_or_404(
+            Diary,
+            slug=self.kwargs['diary_slug'],
+            is_visible=Diary.ALL_CHOICE,
+            is_commentable=Diary.ALL_CHOICE)
 
-		comment_form = CommentForm(self.request.POST)
-		if comment_form.is_valid():
-			new_comment = comment_form.save(commit=False)
-			new_comment.author = self.request.user.profile
-			new_comment.diary = diary
-			new_comment.save()
-			messages.success(self.request, 'Your comment was created successfly.')
-			return redirect(diary)
-		else:
-			return redirect(diary)
+        comment_form = CommentForm(self.request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.author = self.request.user.profile
+            new_comment.diary = diary
+            new_comment.save()
+            diary.comments_count = F('comments_count') + 1
+            diary.save()
+            messages.success(
+                self.request, 'Your comment was created successfly.')
+            return redirect(diary)
+        else:
+            return redirect(diary)
 
 
 class CommentDeleteView(LoginRequiredMixin, DeleteView):
-	model = Comment
-	template_name = 'diaries/comment_delete.html'
-	diary_slug_url_kwarg = 'diary_slug'
-	comment_id_url_kwarg = 'comment_id'
+    model = Comment
+    template_name = 'diaries/comment_delete.html'
+    diary_slug_url_kwarg = 'diary_slug'
+    comment_id_url_kwarg = 'comment_id'
 
-	def get_object(self):
-		obj = get_object_or_404(
-			self.model,
-			id=self.kwargs[self.comment_id_url_kwarg],
-			diary__is_visible=Diary.ALL_CHOICE)
+    def get_object(self):
+        obj = get_object_or_404(
+            self.model,
+            id=self.kwargs[self.comment_id_url_kwarg],
+            diary__is_visible=Diary.ALL_CHOICE)
 
-		if self.request.user.profile == obj.author:
-			return obj
-		raise Http404()
+        if self.request.user.profile == obj.author:
+            return obj
+        raise Http404()
 
-	def get_success_url(self):
-		diary = get_object_or_404(
-			Diary,
-			slug=self.kwargs[self.diary_slug_url_kwarg])
-		messages.warning(self.request, 'Your comment was deleted successfly.')
-		return diary.get_absolute_url()
+    def get_success_url(self):
+        diary = get_object_or_404(
+            Diary,
+            slug=self.kwargs[self.diary_slug_url_kwarg]
+        )
+        # Not sure if i should put this here
+        diary.comments_count = F('comments_count') - 1
+        diary.save()
+        messages.warning(self.request, 'Your comment was deleted successfly.')
+        return diary.get_absolute_url()
 
 
 class SearchView(View):
-	def get(self, request):
-		q = request.GET.get('q', '')
+    def get(self, request):
+        q = request.GET.get('q', '')
 
-		diaries = Diary.objects.with_likes_and_comments_count()\
-			.active(self.request.user)
-		diaries = diaries.filter(title__contains=q).distinct()[:9]
+        diaries = Diary.objects.active(self.request.user)
+        diaries = diaries.filter(title__contains=q).distinct()[:9]
 
-		profiles = Profile.objects.filter(
-			Q(name__contains=q) |
-			Q(user__username__contains=q) |
-			Q(description__contains=q)).distinct()[:12]
+        profiles = Profile.objects.filter(
+            Q(name__contains=q) |
+            Q(user__username__contains=q) |
+            Q(description__contains=q)).distinct()[:12]
 
-		context = {
-			'diaries': diaries,
-			'profiles': profiles,
-			'q': q
-		}
-		return render(request, 'diaries/search.html', context)
+        context = {
+            'diaries': diaries,
+            'profiles': profiles,
+            'q': q
+        }
+        return render(request, 'diaries/search.html', context)
 
 
 class NotificationListView(LoginRequiredMixin, NotificationViewList):
-	template_name = 'diaries/notification_list.html'
-	paginate_by = 10
+    template_name = 'diaries/notification_list.html'
+    paginate_by = 10
 
-	def get_queryset(self):
-		qs = Notification.objects.filter(recipient=self.request.user)
-		qs = qs.active()
-		return qs
+    def get_queryset(self):
+        qs = Notification.objects.filter(recipient=self.request.user)
+        qs = qs.active()
+        return qs
