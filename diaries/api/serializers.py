d2f2843b98805dbdd719bcd5b51a75e0f8bf41fd
Omar <ramiritoramo@gmail.com>
2020-01-03 19:40:42 +0100

    Create Diary and Comment serializers

---
+++ diaries/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from ..models import Diary, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
+    author_image = serializers.ReadOnlyField(source='author.image.url')
+
+    class Meta:
+        model = Comment
+        fields = ['content', 'created_on', 'author_name', 'author_image']
+        readonly_fields = ['created_on']
+
+
+class DiaryListSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
+    author_image = serializers.ReadOnlyField(source='author.image.url')
+
+    class Meta:
+        model = Diary
+        fields = ['title', 'slug', 'image', 'description',
+                  'created_on', 'likes_count', 'comments_count',
+                  'author_name', 'author_image']
+        read_only_fields = ['slug', 'description',
+                            'created_on', 'likes_count', 'comments_count']
+
+
+class DiaryDetailSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.name')
+    author_image = serializers.ReadOnlyField(source='author.image.url')
+    comments = CommentSerializer(many=True)
+
+    class Meta:
+        model = Diary
+        fields = ['title', 'slug', 'image', 'content',
+                  'created_on', 'likes_count', 'comments_count',
+                  'author_name', 'author_image', 'feeling', 'is_commentale']
+        read_only_fields = ['slug', 'created_on',
+                            'likes_count', 'comments_count']
