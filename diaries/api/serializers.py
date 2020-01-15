530d595bd2f6c0442bef90745fd53dd93e24cb2c
Omar <ramiritoramo@gmail.com>
2020-01-15 14:53:16 +0100

    Add 'url', Change how 'feeling' is displayed in DiaryListSerializer/DiaryDetailSerializer

--- diaries/api/serializers.py
+++ diaries/api/serializers.py
@@ -16,13 +16,18 @@ class CommentSerializer(serializers.ModelSerializer):
 class DiaryListSerializer(serializers.ModelSerializer):
     author_name = serializers.ReadOnlyField(source='author.name')
     author_image = serializers.ReadOnlyField(source='author.image.url')
+    feeling = serializers.CharField(source='get_feeling_display')
+    url = serializers.HyperlinkedIdentityField(
+        view_name='diaries_api:diary_detail',
+        lookup_url_kwarg='diary_slug',
+        lookup_field='slug')
 
     class Meta:
         model = Diary
-        fields = ['title', 'slug', 'image', 'description', 'created_on',
+        fields = ['url', 'title', 'slug', 'image', 'description', 'created_on',
                   'likes_count', 'comments_count', 'author_name',
-                  'author_image']
-        read_only_fields = ['slug', 'description',
+                  'author_image', 'feeling']
+        read_only_fields = ['url', 'slug', 'description',
                             'created_on', 'likes_count', 'comments_count']
 
 
@@ -30,11 +35,16 @@ class DiaryDetailSerializer(serializers.ModelSerializer):
     author_name = serializers.ReadOnlyField(source='author.name')
     author_image = serializers.ReadOnlyField(source='author.image.url')
     comments = CommentSerializer(many=True, read_only=True)
-    # lookup_field = 'slug'
+    lookup_field = 'slug'
+    feeling = serializers.CharField(source='get_feeling_display')
+    url = serializers.HyperlinkedIdentityField(
+        view_name='diaries_api:diary_detail',
+        lookup_url_kwarg='diary_slug',
+        lookup_field='slug')
 
     class Meta:
         model = Diary
-        fields = ['title', 'slug', 'image', 'content', 'created_on',
+        fields = ['url', 'title', 'slug', 'image', 'content', 'created_on',
                   'likes_count', 'comments_count', 'author_name',
                   'author_image', 'feeling', 'is_visible', 'is_commentable',
                   'comments']
--- diaries/api/urls.py
+++ diaries/api/urls.py
@@ -7,7 +7,7 @@ app_name = 'diaries_api'
 urlpatterns = [
     path('', views.DiaryListCreateAPIView.as_view(), name='diary_list'),
     path(
-        '<slug:diary_slug>',
+        '<str:diary_slug>',
         views.DiaryRetrieveUpdateDestroyAPIView.as_view(),
         name='diary_detail'),
 ]
