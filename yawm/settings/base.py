8a1d8464e9a1c40ed7dcbe3e38455c08dd70f34c
Omar <ramiritoramo@gmail.com>
2020-02-01 01:03:42 +0100

    Did many things...i think

--- yawm/settings/base.py
+++ yawm/settings/base.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.contrib.messages import constants as messages
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
@@ -173,9 +175,11 @@ CKEDITOR_CONFIGS = {
 
 DJANGO_NOTIFICATIONS_CONFIG = {'SOFT_DELETE': True}
 
-from django.contrib.messages import constants as messages
-
 MESSAGE_TAGS = {
     messages.SUCCESS: 'alert-success',
     messages.WARNING: 'alert-warning'
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning'
 }
